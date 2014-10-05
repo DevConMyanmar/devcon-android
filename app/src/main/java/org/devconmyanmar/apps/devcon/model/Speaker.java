afb789e88f8381f6bd633dd306cb607c99ea8585
Ye Lin Aung <me@yelinaung.com>
2014-10-05 18:48:13 +0630

    add model attributes

--- app/src/main/java/org/devconmyanmar/apps/devcon/model/Speaker.java
+++ app/src/main/java/org/devconmyanmar/apps/devcon/model/Speaker.java
@@ -1,10 +1,58 @@
 package org.devconmyanmar.apps.devcon.model;
 
 import io.realm.RealmObject;
+import java.util.List;
 
 /**
  * Created by Ye Lin Aung on 14/10/05.
  */
 public class Speaker extends RealmObject {
   private int id;
+  private String name;
+  private String title;
+  private String description;
+  private List<Talk> associated_talks;
+
+  public Speaker() {
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public List<Talk> getAssociated_talks() {
+    return associated_talks;
+  }
+
+  public void setAssociated_talks(List<Talk> associated_talks) {
+    this.associated_talks = associated_talks;
+  }
 }
--- app/src/main/java/org/devconmyanmar/apps/devcon/model/Talk.java
+++ app/src/main/java/org/devconmyanmar/apps/devcon/model/Talk.java
@@ -1,7 +1,67 @@
 package org.devconmyanmar.apps.devcon.model;
 
+import io.realm.RealmObject;
+
 /**
  * Created by Ye Lin Aung on 14/10/05.
  */
-public class Talk {
+public class Talk extends RealmObject {
+
+  private int id;
+  private String title;
+  private String description;
+  private String photo;
+  private String date;
+  private boolean favourite;
+
+  public Talk() {
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getPhoto() {
+    return photo;
+  }
+
+  public void setPhoto(String photo) {
+    this.photo = photo;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public boolean isFavourite() {
+    return favourite;
+  }
+
+  public void setFavourite(boolean favourite) {
+    this.favourite = favourite;
+  }
 }
