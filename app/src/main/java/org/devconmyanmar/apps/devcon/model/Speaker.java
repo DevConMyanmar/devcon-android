1dee91d0d893a810c53fd102b8dd6903689965c0
Ye Lin Aung <me@yelinaung.com>
2014-10-05 16:09:03 +0630

    integration realm

---
+++ app/src/main/java/org/devconmyanmar/apps/devcon/model/Speaker.java
@@ -0,0 +1,10 @@
+package org.devconmyanmar.apps.devcon.model;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by Ye Lin Aung on 14/10/05.
+ */
+public class Speaker extends RealmObject {
+  private int id;
+}
---
+++ app/src/main/java/org/devconmyanmar/apps/devcon/model/Talk.java
@@ -0,0 +1,7 @@
+package org.devconmyanmar.apps.devcon.model;
+
+/**
+ * Created by Ye Lin Aung on 14/10/05.
+ */
+public class Talk {
+}
