f6ed889b01870e7cc352fda6570413b51fc1fd49
Ye Lin Aung <me@yelinaung.com>
2014-10-05 11:14:57 +0630

    implementation of nav drawer

--- app/src/main/java/org/devconmyanmar/apps/devcon/ui/ScheduleListActivity.java
+++ app/src/main/java/org/devconmyanmar/apps/devcon/ui/ScheduleListActivity.java
@@ -3,10 +3,17 @@ package org.devconmyanmar.apps.devcon.ui;
 import android.app.ActionBar;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
@@ -20,6 +27,12 @@ public class ScheduleListActivity extends BaseActivity {
 
   @InjectView(R.id.sliding_tabs) SlidingTabLayout mSlidingTabLayout;
   @InjectView(R.id.view_pager) ViewPager mViewPager;
+  @InjectView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
+  @InjectView(R.id.left_drawer) ListView mDrawerList;
+
+  private ActionBarDrawerToggle mDrawerToggle;
+  private String[] mPlanetTitles;
+  private ActionBar mActionBar;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -37,16 +50,69 @@ public class ScheduleListActivity extends BaseActivity {
     mSlidingTabLayout.setDistributeEvenly(true);
     mSlidingTabLayout.setViewPager(mViewPager);
 
-    ActionBar mActionBar = getActionBar();
+    final String mTitle = getString(R.string.schedule_activity);
+    final String mDrawerTitle = getString(R.string.app_name);
+
+    mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+    mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_navigation_drawer,
+        R.string.drawer_open, R.string.drawer_close) {
+
+      public void onDrawerClosed(View view) {
+        super.onDrawerClosed(view);
+        mActionBar.setTitle(mTitle);
+        invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+      }
+
+      public void onDrawerOpened(View drawerView) {
+        super.onDrawerOpened(drawerView);
+        mActionBar.setTitle(mDrawerTitle);
+        invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+      }
+    };
+
+    mDrawerToggle.syncState(); // this is shit
+    mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+    mActionBar = getActionBar();
     if (mActionBar != null) {
       mActionBar.setIcon(android.R.color.transparent);
-      mActionBar.setTitle("        " + getString(R.string.schedule_activity));
+      //mActionBar.setTitle(getString(R.string.schedule_activity));
+      mActionBar.setDisplayHomeAsUpEnabled(true);
+      mActionBar.setHomeButtonEnabled(true);
     }
 
     SystemBarTintManager tintManager = new SystemBarTintManager(this);
     tintManager.setStatusBarTintEnabled(true);
     tintManager.setNavigationBarTintEnabled(false);
     tintManager.setTintColor(getResources().getColor(R.color.translucent_actionbar_background));
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.schedule_menu, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  /* Called whenever we call invalidateOptionsMenu() */
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    // If the nav drawer is open, hide action items related to the content view
+    boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+    menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+    return super.onPrepareOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // The action bar home/up action should open or close the drawer.
+    // ActionBarDrawerToggle will take care of this.
+    if (mDrawerToggle.onOptionsItemSelected(item)) {
+      return true;
+    }
+    // Handle action buttons
+    switch (item.getItemId()) {
+      default:
+        return super.onOptionsItemSelected(item);
+    }
   }
 
   private class SlidingTabAdapter extends FragmentPagerAdapter {
