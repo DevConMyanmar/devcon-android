2ce59531de43861bb52559e3deb448459ea527f6
Ye Lin Aung <me@yelinaung.com>
2014-11-11 11:52:39 +0630

    bye bye facebook

--- app/src/main/java/org/devconmyanmar/apps/devcon/DevConApplication.java
+++ app/src/main/java/org/devconmyanmar/apps/devcon/DevConApplication.java
@@ -7,9 +7,6 @@ import com.bumptech.glide.integration.okhttp.OkHttpUrlLoader;
 import com.bumptech.glide.load.engine.cache.DiskLruCacheWrapper;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.squareup.okhttp.OkHttpClient;
-import com.sromku.simple.fb.Permission;
-import com.sromku.simple.fb.SimpleFacebook;
-import com.sromku.simple.fb.SimpleFacebookConfiguration;
 import java.io.InputStream;
 
 /**
@@ -22,19 +19,6 @@ public class DevConApplication extends Application {
 
   @Override public void onCreate() {
     super.onCreate();
-
-    Permission[] permissions = new Permission[] {
-        Permission.EMAIL,
-        Permission.PUBLISH_ACTION
-    };
-
-    SimpleFacebookConfiguration configuration = new SimpleFacebookConfiguration.Builder()
-        .setAppId("719288351499456")
-        .setNamespace("devcon")
-        .setPermissions(permissions)
-        .build();
-
-    SimpleFacebook.setConfiguration(configuration);
 
     okHttpClient = new OkHttpClient();
 
