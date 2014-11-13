f1d1cd0d19ef2fcf608a4023d1b37f2c6c80512d
Ye Lin Aung <me@yelinaung.com>
2014-11-13 17:30:30 +0630

    enable crashlytics, and bump to 1.0.0 !

--- app/src/main/java/org/devconmyanmar/apps/devcon/DevConApplication.java
+++ app/src/main/java/org/devconmyanmar/apps/devcon/DevConApplication.java
@@ -6,7 +6,9 @@ import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.integration.okhttp.OkHttpUrlLoader;
 import com.bumptech.glide.load.engine.cache.DiskLruCacheWrapper;
 import com.bumptech.glide.load.model.GlideUrl;
+import com.crashlytics.android.Crashlytics;
 import com.squareup.okhttp.OkHttpClient;
+import io.fabric.sdk.android.Fabric;
 import java.io.InputStream;
 
 /**
@@ -19,7 +21,9 @@ public class DevConApplication extends Application {
 
   @Override public void onCreate() {
     super.onCreate();
-    //Fabric.with(this, new Crashlytics());
+    if (BuildConfig.DEBUG) {
+      Fabric.with(this, new Crashlytics());
+    }
 
     okHttpClient = new OkHttpClient();
 
