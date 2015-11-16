666c23296bdffc4b46ab9979cf16d5cf66d43d42
Ye Lin Aung <me@yelinaung.com>
2015-11-16 16:01:58 +0630

    New app icon

--- app/src/main/java/org/devconmyanmar/apps/devcon/DevConApplication.java
+++ app/src/main/java/org/devconmyanmar/apps/devcon/DevConApplication.java
@@ -25,36 +25,16 @@
 package org.devconmyanmar.apps.devcon;
 
 import android.app.Application;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.integration.okhttp.OkHttpUrlLoader;
-import com.bumptech.glide.load.engine.cache.DiskLruCacheWrapper;
-import com.bumptech.glide.load.model.GlideUrl;
-import com.squareup.okhttp.OkHttpClient;
-import java.io.InputStream;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Ye Lin Aung on 14/10/04.
  */
 public class DevConApplication extends Application {
 
-  public static GlideBuilder builder;
-  public static OkHttpClient okHttpClient;
-
   @Override public void onCreate() {
     super.onCreate();
-    if (!BuildConfig.DEBUG) {
-      //Fabric.with(this, new Crashlytics());
-    }
-
-    okHttpClient = new OkHttpClient();
-
-    builder = new GlideBuilder(this).setDiskCache(
-        DiskLruCacheWrapper.get(Glide.getPhotoCacheDir(this), 50 * 1024 * 1024));
-    Glide.get(this)
-        .register(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(okHttpClient));
-    if (!Glide.isSetup()) {
-      Glide.setup(builder);
-    }
+    Fabric.with(this, new Crashlytics());
   }
 }
